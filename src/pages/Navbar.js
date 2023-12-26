8f93394b527d04baf549aac206a4fd3b1fd0e5d5
seansongss <seansongss@gmail.com>
2023-12-26 10:18:51 -0500

    minor navbar change

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -4,13 +4,22 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <>
-        <nav className='navbar'>
-            <div className='navbar-container'>
-                <Link to='/'>Home</Link>
-                <Link to='/aboutme'>About Me</Link>
-                <Link to='/hobbies'>Hobbies</Link>
-            </div>
-        </nav>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo'>seansongss</Link>
+          <ul className='navbar-list'>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/aboutme'>About Me</Link>
+            </li>
+            <li>
+              <Link to='/hobbies'>Hobbies</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </>
   )
 }
