58dd63e07c0f9aa2d50ef29291417ebbc906639f
seansongss <seansongss@gmail.com>
2023-12-28 03:04:17 -0500

    Updates

---
+++ precache-manifest.e84a6cf29ddbe587d6b95295b35b5602.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ee242b63c5cfcaf55b450612f29241",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e24ed630e8df85c71a3",
+    "url": "/static/css/main.468d11a7.chunk.css"
+  },
+  {
+    "revision": "f989dd9302e633b1937c",
+    "url": "/static/js/2.75e76385.chunk.js"
+  },
+  {
+    "revision": "8b8df50ef09d07a7afe17ccff91b3404",
+    "url": "/static/js/2.75e76385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e24ed630e8df85c71a3",
+    "url": "/static/js/main.950ec557.chunk.js"
+  },
+  {
+    "revision": "f08378fd451f02ca3215",
+    "url": "/static/js/runtime-main.ba08db2b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4245fe76fbb67433709b99b1537e2100.js"
+  "/precache-manifest.e84a6cf29ddbe587d6b95295b35b5602.js"
 );
 
 self.addEventListener('message', (event) => {
