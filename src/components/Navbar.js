77b2c2800392411876382858684a1c7fbf0110d5
seansongss <seansongss@gmail.com>
2024-01-02 01:58:05 -0500

    clickable navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,19 +11,19 @@ export default function Navbar() {
         <div className='navbar-links-container'>
           <ul className='navbar-list'>
             <li>
-              <Link className='navbar-component' to="home" spy={true} smooth={true} offset={-80} duration={800}>Home</Link>
+              <Link to="home" spy={true} smooth={true} offset={-80} duration={800}>Home</Link>
             </li>
             <li>
-              <Link className='navbar-component' to="about" spy={true} smooth={true} offset={-80} duration={800}>About</Link>
+              <Link to="about" spy={true} smooth={true} offset={-80} duration={800}>About</Link>
             </li>
             <li>
-              <Link className='navbar-component' to="projects" spy={true} smooth={true} offset={-80} duration={800}>Projects</Link>
+              <Link to="projects" spy={true} smooth={true} offset={-80} duration={800}>Projects</Link>
             </li>
             <li>
-              <Link className='navbar-component' to="experience" spy={true} smooth={true} offset={-80} duration={800}>Experience</Link>
+              <Link to="experience" spy={true} smooth={true} offset={-80} duration={800}>Experience</Link>
             </li>
             <li>
-              <Link className='navbar-component' to="contact" spy={true} smooth={true} offset={-80} duration={800}>Contact</Link>
+              <Link to="contact" spy={true} smooth={true} offset={-80} duration={800}>Contact</Link>
             </li>
           </ul>
         </div>
