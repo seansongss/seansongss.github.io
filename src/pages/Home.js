fbcb834606931d1cf48d8b85445962b75b38da10
seansongss <seansongss@gmail.com>
2024-01-09 01:10:23 -0500

    background slider animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,58 @@
 import React from 'react';
+import { motion } from 'framer-motion/dist/framer-motion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAnglesDown } from '@fortawesome/free-solid-svg-icons';
+
+import './Home.css';
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+
+const sliderVariants = {
+  initial: {
+    x: 100,
+  },
+  animate: {
+    x: '-200%',
+    transition: {
+      repeat: Infinity,
+      repeatType: 'mirror',
+      duration: 20,
+    },
+  },
+}
 
 const Home = () => {
   return (
     <div id='home' className='page'>
-      <h1>Home Page</h1>
-      <p>Welcome to my website!</p>
+      <motion.div className='text-container' variants={textVariants} initial='initial' animate='animate'>
+        <motion.h1 variants={textVariants}>SEAN SONG</motion.h1>
+        <motion.h2 variants={textVariants}>An enthusiatic university student</motion.h2>
+        <motion.div variants={textVariants} animate='scrollButton' style={{ cursor: 'pointer' }}><FontAwesomeIcon icon={faAnglesDown} size='2x' /></motion.div>
+      </motion.div>
+      <div className='slider-wrapper'>
+        <motion.div className='slider-text-container' variants={sliderVariants} initial='initial' animate='animate' style={{ cursor: 'default'}}>Engineer Developer Reader Skier</motion.div>
+      </div>
     </div>
   );
 };
