2c9354f2c5246d95d6fcd9500b61c660fbd35700
seansongss <seansongss@gmail.com>
2024-01-13 04:52:04 -0500

    Updates

---
+++ precache-manifest.95855866df75e6a7daa2b5eded9641d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a344db1aee1f2509e82501a69c656b2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b37227f5b416cd8dde4f",
+    "url": "/static/css/main.eacb19a8.chunk.css"
+  },
+  {
+    "revision": "44a889e53f42f84c3d9f",
+    "url": "/static/js/2.afa6f90d.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.afa6f90d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b37227f5b416cd8dde4f",
+    "url": "/static/js/main.fe41a5dc.chunk.js"
+  },
+  {
+    "revision": "f08378fd451f02ca3215",
+    "url": "/static/js/runtime-main.ba08db2b.js"
+  }
+]);
